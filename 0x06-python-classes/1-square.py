751c51ce9ce2177181ddbdc3a1170e790f60e2ec
KCael <katenganga22@gmail.com>
2022-06-21 19:58:25 -0700

    Based on 0-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Defines an empty square."""
+
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size):
+        """Initializes the data.
+        Private instance attribute: size.
+        """
+        self.__size = size 
