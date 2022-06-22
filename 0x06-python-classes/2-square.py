2d2aef602ceeac1350a65de176073af925bdd9d6
KCael <katenganga22@gmail.com>
2022-06-21 20:00:10 -0700

    Based on 1-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines an empty square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initializes the data.
+        Private instance attribute: size.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
