94387a2c4d3c4acf5a88d74f4b1e5d679910e78a
KCael <katenganga22@gmail.com>
2022-07-05 19:07:34 -0700

    returns the JSON representation of an object

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""returns the JSON representation"""
+
+import json
+
+
+def to_json_string(my_obj):
+    """JSON representation of an object"""
+
+    return json.dumps(my_obj)
