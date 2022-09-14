f8a5c3a9c5f5e2923477b18ee5ebb7130ddba6e3
KCael <katenganga22@gmail.com>
2022-09-14 15:34:05 -0700

    Corrections Task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@
 class Square:
     """Represents a square."""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initializes the data.
         Private instance attribute: size.
         """
