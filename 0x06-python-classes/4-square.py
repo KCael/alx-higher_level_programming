5558bc3369d879f110591b84c00fb48c7d4dcf18
KCael <katenganga22@gmail.com>
2022-09-14 19:00:44 -0700

    Task 4 - Access and update private attribute

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,7 +11,7 @@ class Square():
     """Retrieves the property"""
 
     def size(self):
-        return self.__size
+        return (self.__size)
 
     """Property setter"""
 
@@ -22,7 +22,7 @@ class Square():
             raise ValueError("size must be >= 0")
         self.__size = value
 
-    """returns current square"""
+    """returns current area of the square"""
 
     def area(self):
         return (self.__size * self_size)
