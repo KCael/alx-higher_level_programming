79165d7c211ba4e225484fa057bbbb5ebd85ee20
KCael <katenganga22@gmail.com>
2022-11-02 19:58:22 -0700

    Add SQL tasks

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,12 @@
+-- Creates the database hbtn_0d_2
+-- If db exists, script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user user_0d_2
+-- The user_0d_2 password should be set to user_0d_2_pwd
+-- If user already exists, script should not fail 
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- user_0d_2 should have only SELECT privilege in the database hbtn_0d_2
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
