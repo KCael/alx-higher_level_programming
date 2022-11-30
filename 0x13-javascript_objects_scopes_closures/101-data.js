c7183b8e7ef86f9cf9d50dd461358f229b66c6a0
KCael <katenganga22@gmail.com>
2022-11-30 03:00:59 +0000

    Add tasks

---
+++ 0x13-javascript_objects_scopes_closures/101-data.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/**
+ * An arbitrary object with keys as occurrences and their
+ * corresponding value as a user id.
+ */
+exports.dict = {
+  60: 1,
+  61: 2,
+  62: 1,
+  63: 7,
+  64: 4,
+  65: 3
+};
