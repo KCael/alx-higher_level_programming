dc1c29b1c3012f1c463bf092e560ae588202c3bb
KCael <katenganga22@gmail.com>
2022-11-30 12:37:32 +0000

    Add tasks

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-myObject.incr = function () {
-  this.value++;
-};
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
-myObject.incr();
-console.log(myObject);
+const fs = require('fs');
+
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
+
+if ((fs.existsSync(fileA) && fs.statSync(fileA).isFile) &&
+  (fs.existsSync(fileB) && fs.statSync(fileB).isFile) &&
+  (fileC !== undefined)) {
+  const fileAContent = fs.readFileSync(fileA);
+  const fileBContent = fs.readFileSync(fileB);
+  const stream = fs.createWriteStream(fileC);
+
+  stream.write(fileAContent);
+  stream.write(fileBContent);
+  stream.end();
+}
